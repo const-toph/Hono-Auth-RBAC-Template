045879c8151309141c01e8c0fe8fa6aaae23845a
topherism <christophermanubay04@gmail.com>
2025-11-24 20:26:03 +0800

    chore: remove the log of req

--- backend/src/middlewares/pino-logger.ts
+++ backend/src/middlewares/pino-logger.ts
@@ -8,8 +8,13 @@ import pretty from "pino-pretty";
 export function pinoLogger() {
   return logger({
     pino: pino(
-      // Customize the log level shown based on the environment variable
-      { level: envConfig.LOG_LEVEL || "info" }, // default to 'info' if LOG_LEVEL is not set
+      { 
+        level: envConfig.LOG_LEVEL || "info",
+        // Use serializers to remove req
+        serializers: {
+          // req: () => undefined,
+        },
+      },                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
       envConfig.NODE_ENV === "production" ? undefined : pretty()
     ),
     http: {
